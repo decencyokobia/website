3bea61c8806aaa8ac41df9b0dccfc05450d18296
Decency Okobia <learncodingdee@yahoo.com>
2025-04-21 11:10:29 +0100

    added close and menu bar to all pages

--- assets/js/pagenav.js
+++ assets/js/pagenav.js
@@ -1,24 +1,37 @@
-const menuBar = document.getElementById('menuBar');
-const closeBar = document.getElementById('closeBar');
-const sideBar = document.getElementById('mySidebar-links');
+const menuBars = document.querySelectorAll('.menuBar');
+const closeBars = document.querySelectorAll('.closeBar');
+const sideBars = document.querySelectorAll('.mySidebar-links');
 const sideBarItems = document.querySelectorAll('.mySidebar-item')
 
-closeBar.addEventListener('click', () => {
-    sideBar.style.display = 'none';
-    menuBar.style.display = 'flex';
-    closeBar.style.display = 'none';
-});
-
-menuBar.addEventListener('click', () => {
-    sideBar.style.display = 'flex';
-    closeBar.style.display = 'flex';
-    menuBar.style.display = 'none';
-});
-
-sideBarItems.addEventListener('click', () => {
-    sideBarItems.forEach(sideBarItem => {
-        sideBarItem.addEventListener('click', () => {
-            sideBar.style.display = 'none'
+menuBars.forEach(menuBar => {
+    menuBar.addEventListener('click', () => {
+        sideBars.forEach(sideBar => {
+            sideBar.style.display = 'flex';
         })
+        closeBars.forEach(closeBar => {
+            closeBar.style.display = 'flex';
+        })
+        menuBar.style.display = 'none';
+    });
+});
+
+closeBars.forEach(closeBar => {
+    closeBar.addEventListener('click', () => {
+        sideBars.forEach(sideBar => {
+            sideBar.style.display = 'none';
+        })
+      menuBars.forEach(menuBar => {
+        menuBar.style.display = 'flex'
+      })
+        closeBar.style.display = 'none';
+    });
+});
+
+
+sideBarItems.forEach(sideBarItem => {
+    sideBarItem.addEventListener('click', () => {
+        sideBar.style.display = 'none';
+        menuBar.style.display = 'flex';
+        closeBar.style.display = 'none';
     })
 })
